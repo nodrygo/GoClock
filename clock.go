a8cef75d38af827b79cad87883e8d519b7b707c4
nodrygo <yvan.godin@gmail.com>
2019-04-22 15:10:01 +0200

    use color names

--- clock.go
+++ clock.go
@@ -11,6 +11,10 @@ import (
 	"github.com/gotk3/gotk3/cairo"
 	"github.com/gotk3/gotk3/gdk"
 	"github.com/gotk3/gotk3/gtk"
+
+	"image/color"
+
+	"golang.org/x/image/colornames"
 )
 
 const (
@@ -35,15 +39,40 @@ func getWinSize(win *gtk.Window) (float64, float64, float64) {
 	return wfx, wfy, radius
 }
 
+// convert named color to RGB as flot 64 between 0..1 needed for cairo cr
+func colorConvert(c color.RGBA) (r, g, b float64) {
+	//fmt.Println("Color is ", c)
+	cr, cg, cb := c.R, c.G, c.B
+	//fmt.Printf("R %d, G %d B %d   ", cr, cb, cg)
+	r = float64(cr) / 255.0
+	g = float64(cg) / 255.0
+	b = float64(cb) / 255.0
+	return
+}
+
+// prepare color for stop gradian as color name
+func stopColorConvertAlpha(stop float64, c color.RGBA, alpha float64) (s, r, g, b, a float64) {
+	//fmt.Println("Color is ", c)
+	cr, cg, cb := colorConvert(c)
+	//fmt.Printf("R %d, G %d B %d   ", cr, cb, cg)
+	r = cr
+	g = cg
+	b = cb
+	s = stop
+	a = alpha
+
+	return
+}
+
 // draw neddles at angle stat at cx,cy for length cl and width siz
 func drawNeddle(cr *cairo.Context, cx, cy, cl, siz, angle float64) {
 	cr.Save()
 	cr.MoveTo(cx, cy)
-	cr.SetSourceRGB(0, 0, 0)
+	r, g, b := colorConvert(colornames.Chocolate)
+	//fmt.Println("color", r, " ", b, " ", g)
+	cr.SetSourceRGBA(r, g, b, 0.8)
 	cr.SetLineWidth(siz)
-
-	cr.LineTo(cx+cl*math.Sin(angle),
-		cy+cl*math.Cos(angle))
+	cr.LineTo(cx+cl*math.Sin(angle), cy+cl*math.Cos(angle))
 	cr.Stroke()
 	cr.Restore()
 }
@@ -54,11 +83,12 @@ func drawMinutes(cr *cairo.Context, cx, cy, radius float64) {
 	lend := lstart + radius/10
 	cr.Save()
 	cr.SelectFontFace("Helvetica", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
-	cr.SetFontSize(radius / 10)
+	cr.SetFontSize(radius / 8)
 	cr.MoveTo(cx, cy)
-	cr.SetSourceRGB(0, 0, 0)
+
 	cr.SetLineWidth(3)
 	for i := 12; i > 0; i-- {
+		cr.SetSourceRGB(0, 0, 0)
 		angle := (float64(i*5) * -(math.Pi / 30.0)) + math.Pi
 		cr.MoveTo(cx+lstart*math.Sin(angle), cy+lstart*math.Cos(angle))
 		cr.LineTo(cx+lend*math.Sin(angle),
@@ -88,7 +118,7 @@ func drawMinutes(cr *cairo.Context, cx, cy, radius float64) {
 		case 12:
 			cr.MoveTo(cx-tdw+lend*math.Sin(angle), cy+(tdh+lend)*math.Cos(angle))
 		}
-
+		cr.SetSourceRGB(colorConvert(colornames.Darkslateblue))
 		cr.ShowText(fmt.Sprint(i))
 	}
 	cr.Restore()
@@ -97,14 +127,19 @@ func drawMinutes(cr *cairo.Context, cx, cy, radius float64) {
 // draw clock face
 func drawFace(cr *cairo.Context, cx, cy, radius float64) {
 	h := 0
-	cr.SetSourceRGB(255, 255, 255)
+	cr.SetSourceRGBA(255, 255, 255, 255)
 	cr.Paint()
+
+	cr.SetSourceRGB(colorConvert(colornames.Chocolate))
 	cr.ShowText(string(h))
-	cr.SetSourceRGB(255, 0, 0)
+	// set color gradian
 	p, _ := cairo.NewPatternLinear(0, 0, cx+radius, cy+radius)
-	p.AddColorStopRGBA(0, 255, 0, 0, 0.3)
-	p.AddColorStopRGBA(0.5, 255, 255, 0, 0.3)
-	p.AddColorStopRGBA(1, 255, 0, 255, 0.3)
+	p.AddColorStopRGBA(stopColorConvertAlpha(0.0, colornames.Darkred, 0.3))
+	p.AddColorStopRGBA(stopColorConvertAlpha(0.25, colornames.Red, 0.3))
+	p.AddColorStopRGBA(stopColorConvertAlpha(0.45, colornames.Green, 0.3))
+	p.AddColorStopRGBA(stopColorConvertAlpha(0.60, colornames.Darkgreen, 0.3))
+	p.AddColorStopRGBA(stopColorConvertAlpha(0.8, colornames.Blue, 0.3))
+	p.AddColorStopRGBA(stopColorConvertAlpha(1.0, colornames.Darkblue, 0.3))
 	cr.SetSource(p)
 	cr.Arc(cx, cy, radius, 0, math.Pi*2)
 	cr.Fill()
@@ -130,6 +165,7 @@ func drawClock(cr *cairo.Context) {
 	cy := wfy / 2
 	// draw clock face
 	drawFace(cr, cx, cy, maxradius)
+
 	// draw second
 	drawNeddle(cr, cx, cy, maxradius-10.0, 2.0, sangle)
 	// draw min
@@ -140,7 +176,10 @@ func drawClock(cr *cairo.Context) {
 	//draw center point
 	cr.Arc(cx, cy, maxradius/14, 0, math.Pi*2)
 	cr.Fill()
+
 	// draw digital clock inside clock
+	r, g, b := colorConvert(colornames.Darkblue)
+	cr.SetSourceRGB(r, g, b)
 	cr.SelectFontFace("Helvetica", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
 	cr.SetFontSize(radius / 5)
 	txttime := fmt.Sprintf("%02d:%02d:%02d", hour, min, sec)
@@ -196,7 +235,7 @@ func main() {
 	win.ShowAll()
 	popupmenu.ShowAll()
 
-	win.SetOpacity(0.8)
+	win.SetOpacity(0.9)
 	clkcanvas.SetOpacity(1)
 	wfx, wfy, radius = getWinSize(win)
 	lastTime = time.Now()
@@ -210,10 +249,12 @@ func main() {
 		fmt.Println("menuitem pswitchDeco")
 		if win.GetDecorated() {
 			win.SetDecorated(false)
-			win.SetOpacity(0.8)
+			win.SetOpacity(0.9)
+			clkcanvas.SetOpacity(1)
 		} else {
 			win.SetDecorated(true)
-			win.SetOpacity(0.8)
+			win.SetOpacity(0.9)
+			clkcanvas.SetOpacity(1)
 		}
 	})
 
