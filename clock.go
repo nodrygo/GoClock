03ce7c5169a19e3dcef32565f383543d5a9cd5ab
nodrygo <yvan.godin@gmail.com>
2019-04-23 14:18:16 +0200

    simplify math by use of cairo rotation

--- clock.go
+++ clock.go
@@ -64,78 +64,99 @@ func stopColorConvertAlpha(stop float64, c color.RGBA, alpha float64) (s, r, g, 
 	return
 }
 
-// draw neddles at angle stat at cx,cy for length cl and width siz
-func drawNeddle(cr *cairo.Context, cx, cy, cl, siz float64, col color.RGBA, angle float64) {
+func drawHMneddle(cr *cairo.Context, cx, cy, cl, siz float64, col color.RGBA, angle float64) {
 	cr.Save()
-	cr.MoveTo(cx, cy)
+	cr.Translate(cx, cy)
+	cr.Rotate(angle)
 	r, g, b := colorConvert(col)
-	//fmt.Println("color", r, " ", b, " ", g)
-	cr.SetSourceRGBA(r, g, b, 0.8)
+	cr.SetSourceRGBA(r, g, b, 0.55)
 	cr.SetLineWidth(siz)
-	cr.LineTo(cx+cl*math.Sin(angle), cy+cl*math.Cos(angle))
-	cr.Stroke()
-	//cr.NewPath()
-	//cr.MoveTo(cx, cy)
-	//r, g, b = colorConvert(colornames.Coral)
-	//fmt.Println("color", r, " ", b, " ", g)
-	cr.SetSourceRGBA(r, g, b, 0.8)
-	cr.Arc(cx+(cl/1.8)*math.Sin(angle), cy+(cl/1.8)*math.Cos(angle), cx/20, 0, math.Pi*2)
-	//cr.ClosePath()
+	cr.MoveTo(0, 0)
+	cr.LineTo(cl/3, cl/10)
+	cr.LineTo(cl, 0)
+	cr.LineTo(cl/3, -cl/10)
+	cr.LineTo(0, 0)
 	cr.Fill()
 	cr.Restore()
 }
 
-// draw face minutes decorations for each 5mn center cx,cy with radius
-func drawMinutes(cr *cairo.Context, cx, cy, radius float64) {
+// draw neddles at angle stat at cx,cy for length cl and width siz
+func drawSneddle(cr *cairo.Context, cx, cy, cl, siz float64, col color.RGBA, angle float64) {
+	cr.Save()
+	cr.Translate(cx, cy)
+	cr.Rotate(angle)
+	r, g, b := colorConvert(col)
+	cr.SetSourceRGBA(r, g, b, 0.45)
+	cr.SetLineWidth(siz)
+	cr.MoveTo(0, 0)
+	cr.LineTo(cl, 0)
+	cr.Stroke()
+	cr.Arc(cl/1.4, 0, cl/30+siz, 0, math.Pi*2)
+	cr.Fill()
+	cr.Restore()
+}
+
+// draw graduations and texts faces
+func drawGraduations(cr *cairo.Context, cx, cy, radius float64) {
 	lstart := radius - radius/3.8
 	lend := lstart + radius/10
-	cr.Save()
+	fsize := radius / 8
 	cr.SelectFontFace("Helvetica", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
-	cr.SetFontSize(radius / 8)
-	cr.MoveTo(cx, cy)
-
+	cr.SetFontSize(fsize)
 	cr.SetLineWidth(3)
-	for i := 12; i > 0; i-- {
-		cr.SetSourceRGB(0, 0, 0)
-		angle := (float64(i*5) * -(math.Pi / 30.0)) + math.Pi
-		cr.MoveTo(cx+lstart*math.Sin(angle), cy+lstart*math.Cos(angle))
-		cr.LineTo(cx+lend*math.Sin(angle),
-			cy+lend*math.Cos(angle))
+
+	for i := 1; i < 13; i++ {
+		cr.Save()
+		angle := (float64(i*5) * (math.Pi / 30.0)) - math.Pi/2
+		cr.Translate(cx, cy)
+		cr.Rotate(angle)
+		cr.MoveTo(lstart, 0)
+		cr.LineTo(lend, 0)
 		cr.Stroke()
-		tsize := cr.TextExtents(fmt.Sprint(i))
-		tdh := tsize.Height / 2
-		tdw := tsize.Width / 2
+
+		txt := fmt.Sprint(i)
+		tsize := cr.TextExtents(txt)
+		tdh := tsize.Height
+		tdw := tsize.Width
+
 		// adapt text pos according minutes pos and text size
 		switch i {
 		default:
-			cr.MoveTo(cx+(lend-tdw)*math.Sin(angle), cy+(lend+tdh)*math.Cos(angle))
+			cr.MoveTo(lend+tdw, tdh)
 		case 1, 2:
-			cr.MoveTo(cx+tdw+lend*math.Sin(angle), cy+(tdh+lend)*math.Cos(angle))
+			cr.MoveTo(lend+tdw/2, tdh/2)
 		case 3:
-			cr.MoveTo(cx+tdw+lend*math.Sin(angle), cy+tdh+lend*math.Cos(angle))
-		case 4, 5:
-			cr.MoveTo(cx+tdw+lend*math.Sin(angle), cy+4.0+tdh+lend*math.Cos(angle))
+			cr.MoveTo(lend+tdw/2, tdh/2)
+		case 4:
+			cr.MoveTo(lend+tdw, tdh/2)
+		case 5:
+			cr.MoveTo(lend+tdw*1.5, tdh/3)
 		case 6:
-			cr.MoveTo(cx-tdw+lend*math.Sin(angle), cy+2+tdh*2+lend*math.Cos(angle))
-		case 7, 8:
-			cr.MoveTo(cx+(tdw*2+4.0+lend)*math.Sin(angle), cy+(tdh*2+4.0+lend)*math.Cos(angle))
+			cr.MoveTo(lend+tdw+fsize/2, tdh/2)
+		case 7:
+			cr.MoveTo(lend+tdw*2, tdh/3)
+		case 8:
+			cr.MoveTo(lend+tdw*2, -tdh/3)
 		case 9:
-			cr.MoveTo(cx+(tdw*2+4.0+lend)*math.Sin(angle), cy+tdh+lend*math.Cos(angle))
-		case 10, 11:
-			cr.MoveTo(cx+(tdw*2+4.0+lend)*math.Sin(angle), cy+(tdh+lend)*math.Cos(angle))
+			cr.MoveTo(lend+tdw*1.5, -tdh/2)
+		case 10:
+			cr.MoveTo(lend+tdw, -tdh)
+		case 11:
+			cr.MoveTo(lend+tdw/1.5, -tdh/1.5)
 		case 12:
-			cr.MoveTo(cx-tdw+lend*math.Sin(angle), cy+(tdh+lend)*math.Cos(angle))
+			cr.MoveTo(lend+tdw/3, -tdh/3)
 		}
+		cr.Rotate(-angle)
 		cr.SetSourceRGB(colorConvert(colornames.Darkslateblue))
-		cr.ShowText(fmt.Sprint(i))
+		cr.ShowText(txt)
+		cr.Restore()
 	}
-	cr.Restore()
 }
 
 // draw clock face
 func drawFace(cr *cairo.Context, cx, cy, radius float64) {
 	h := 0
-	cr.SetSourceRGBA(255, 255, 255, 255)
+	cr.SetSourceRGBA(255, 255, 255, 0)
 	cr.Paint()
 
 	cr.SetSourceRGB(colorConvert(colornames.Chocolate))
@@ -155,32 +176,33 @@ func drawFace(cr *cairo.Context, cx, cy, radius float64) {
 	cr.SetSourceRGB(0, 0, 0)
 	cr.Arc(cx, cy, radius, 0, math.Pi*2)
 	cr.Stroke()
-	drawMinutes(cr, cx, cy, radius)
-
+	drawGraduations(cr, cx, cy, radius)
 }
 
 // main draw fct when canvas need redraw
 func drawClock(cr *cairo.Context) {
 	hour, min, sec := lastTime.Clock()
 	yy, mm, dd := lastTime.Date()
-	pi := math.Pi
-	//halfpi := pi / 2
-	hangle := pi + (float64(hour+min/90) * -(pi / 6.0))
-	mangle := pi + (float64(min) * -(pi / 30.0))
-	sangle := pi + (float64(sec) * -(pi / 30.0))
+	hangle := (float64(hour+min/90) * (math.Pi / 6.0)) - math.Pi/2
+	mangle := (float64(min) * (math.Pi / 30.0)) - math.Pi/2
+	sangle := (float64(sec) * (math.Pi / 30.0)) - math.Pi/2
 	maxradius := radius - 4
 	cx := wfx / 2
 	cy := wfy / 2
+
 	// draw clock face
 	drawFace(cr, cx, cy, maxradius)
 
 	// draw second
-	drawNeddle(cr, cx, cy, maxradius-10.0, 2.0, colornames.Chocolate, sangle)
+	drawSneddle(cr, cx, cy, maxradius-10.0, 2.0, colornames.Chocolate, sangle)
+
 	// draw min
-	drawNeddle(cr, cx, cy, maxradius-(maxradius/3), 4.0, colornames.Maroon, mangle)
+	drawHMneddle(cr, cx, cy, maxradius-(maxradius/3), 3.0, colornames.Darkgoldenrod, mangle)
+
 	// draw hour
-	drawNeddle(cr, cx, cy, maxradius-(maxradius/2), 8.0, colornames.Firebrick, hangle)
+	drawHMneddle(cr, cx, cy, maxradius-(maxradius/2), 2.0, colornames.Firebrick, hangle)
 	cr.SetSourceRGB(0, 0, 0)
+
 	//draw center point
 	cr.Arc(cx, cy, maxradius/14, 0, math.Pi*2)
 	cr.Fill()
@@ -190,12 +212,14 @@ func drawClock(cr *cairo.Context) {
 	cr.SetSourceRGB(r, g, b)
 	cr.SelectFontFace("Helvetica", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
 	cr.SetFontSize(radius / 5)
+
 	txttime := fmt.Sprintf("%02d:%02d:%02d", hour, min, sec)
 	tsize := cr.TextExtents(txttime)
 	tdh := tsize.Height / 2
 	tdw := tsize.Width / 2
 	cr.MoveTo(cx-tdw, cy+maxradius/2.8-tdh)
 	cr.ShowText(txttime)
+
 	txtdate := fmt.Sprintf("%02d/%02d/%4d", dd, mm, yy)
 	tsize = cr.TextExtents(txtdate)
 	tdh = tsize.Height / 2
@@ -224,7 +248,7 @@ func main() {
 	win.Add(clkcanvas)
 	win.SetTitle("Clock")
 	win.Connect("destroy", gtk.MainQuit)
-
+	//win.SetDecorated(false)
 	// define the popp menu
 	popupmenu, _ := gtk.MenuNew()
 	psetAlarm, _ := gtk.MenuItemNew()
@@ -264,15 +288,18 @@ func main() {
 		gtk.MainQuit()
 	})
 	pswitchDeco.Connect("activate", func() {
-		//fmt.Println("menuitem pswitchDeco")
+		/// try some hack to handle the transparency problem after change deco!
+		/// seem to be a GTK BUG
 		if win.GetDecorated() {
 			win.SetDecorated(false)
 			win.SetOpacity(0.9)
+			win.ShowAll()
 			clkcanvas.SetOpacity(1)
 		} else {
 			win.SetDecorated(true)
 			win.SetOpacity(0.9)
 			clkcanvas.SetOpacity(1)
+			win.ShowAll()
 		}
 	})
 
